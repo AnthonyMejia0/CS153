dd566a516088a1e70f120dd1b15e2250e1d69ba5
AnthonyMejia0 <mejia.anthony96@gmail.com>
2020-05-08 23:14:51 -0700

    added lab2 files

---
+++ Lab2/cmake-build-debug/CMakeFiles/xv6.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/mejia/Documents/xv6/bio.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/bio.c.obj"
+  "C:/Users/mejia/Documents/xv6/bootmain.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/bootmain.c.obj"
+  "C:/Users/mejia/Documents/xv6/cat.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/cat.c.obj"
+  "C:/Users/mejia/Documents/xv6/console.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/console.c.obj"
+  "C:/Users/mejia/Documents/xv6/echo.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/echo.c.obj"
+  "C:/Users/mejia/Documents/xv6/exec.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/exec.c.obj"
+  "C:/Users/mejia/Documents/xv6/file.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/file.c.obj"
+  "C:/Users/mejia/Documents/xv6/forktest.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/forktest.c.obj"
+  "C:/Users/mejia/Documents/xv6/fs.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/fs.c.obj"
+  "C:/Users/mejia/Documents/xv6/grep.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/grep.c.obj"
+  "C:/Users/mejia/Documents/xv6/ide.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/ide.c.obj"
+  "C:/Users/mejia/Documents/xv6/init.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/init.c.obj"
+  "C:/Users/mejia/Documents/xv6/ioapic.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/ioapic.c.obj"
+  "C:/Users/mejia/Documents/xv6/kalloc.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/kalloc.c.obj"
+  "C:/Users/mejia/Documents/xv6/kbd.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/kbd.c.obj"
+  "C:/Users/mejia/Documents/xv6/kill.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/kill.c.obj"
+  "C:/Users/mejia/Documents/xv6/lapic.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/lapic.c.obj"
+  "C:/Users/mejia/Documents/xv6/ln.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/ln.c.obj"
+  "C:/Users/mejia/Documents/xv6/log.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/log.c.obj"
+  "C:/Users/mejia/Documents/xv6/ls.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/ls.c.obj"
+  "C:/Users/mejia/Documents/xv6/main.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/main.c.obj"
+  "C:/Users/mejia/Documents/xv6/memide.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/memide.c.obj"
+  "C:/Users/mejia/Documents/xv6/mkdir.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/mkdir.c.obj"
+  "C:/Users/mejia/Documents/xv6/mkfs.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/mkfs.c.obj"
+  "C:/Users/mejia/Documents/xv6/mp.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/mp.c.obj"
+  "C:/Users/mejia/Documents/xv6/picirq.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/picirq.c.obj"
+  "C:/Users/mejia/Documents/xv6/pipe.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/pipe.c.obj"
+  "C:/Users/mejia/Documents/xv6/printf.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/printf.c.obj"
+  "C:/Users/mejia/Documents/xv6/proc.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/proc.c.obj"
+  "C:/Users/mejia/Documents/xv6/rm.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/rm.c.obj"
+  "C:/Users/mejia/Documents/xv6/sh.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/sh.c.obj"
+  "C:/Users/mejia/Documents/xv6/sleeplock.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/sleeplock.c.obj"
+  "C:/Users/mejia/Documents/xv6/spinlock.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/spinlock.c.obj"
+  "C:/Users/mejia/Documents/xv6/stressfs.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/stressfs.c.obj"
+  "C:/Users/mejia/Documents/xv6/string.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/string.c.obj"
+  "C:/Users/mejia/Documents/xv6/syscall.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/syscall.c.obj"
+  "C:/Users/mejia/Documents/xv6/sysfile.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/sysfile.c.obj"
+  "C:/Users/mejia/Documents/xv6/sysproc.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/sysproc.c.obj"
+  "C:/Users/mejia/Documents/xv6/trap.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/trap.c.obj"
+  "C:/Users/mejia/Documents/xv6/uart.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/uart.c.obj"
+  "C:/Users/mejia/Documents/xv6/ulib.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/ulib.c.obj"
+  "C:/Users/mejia/Documents/xv6/umalloc.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/umalloc.c.obj"
+  "C:/Users/mejia/Documents/xv6/usertests.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/usertests.c.obj"
+  "C:/Users/mejia/Documents/xv6/vm.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/vm.c.obj"
+  "C:/Users/mejia/Documents/xv6/wc.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/wc.c.obj"
+  "C:/Users/mejia/Documents/xv6/zombie.c" "C:/Users/mejia/Documents/xv6/cmake-build-debug/CMakeFiles/xv6.dir/zombie.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
